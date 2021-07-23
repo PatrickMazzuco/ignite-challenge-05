847234d31cae50267338c0043273478ad6dbcb78
Patrick Mazzuco <patrick.flores@acad.pucrs.br>
2021-07-23 17:12:23 -0300

    feat: create home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,6 @@
 import { GetStaticProps } from 'next';
+import Head from 'next/head';
+import { FiCalendar, FiUser } from 'react-icons/fi';
 
 import { getPrismicClient } from '../services/prismic';
 
@@ -24,9 +26,83 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home(): JSX.Element {
+  return (
+    <>
+      <Head>
+        <title>Home | spacetraveling</title>
+      </Head>
+      <main className={`${commonStyles.container}`}>
+        <img className={styles.logo} src="/images/logo.svg" alt="logo" />
+        <div className={`${commonStyles.content} ${styles.posts}`}>
+          <a href="#">
+            <strong>Como utilizar hooks</strong>
+            <p>Pensando em sincronização em vez de ciclos de vida.</p>
+            <div>
+              <time>
+                <FiCalendar /> 15 Mar 2021
+              </time>
+              <span>
+                <FiUser /> Joseph Oliveira
+              </span>
+            </div>
+          </a>
+          <a href="#">
+            <strong>Criando um app CRA do zero</strong>
+            <p>
+              Tudo sobre como criar a sua primeira aplicação utilizando Create
+              React App.
+            </p>
+            <div>
+              <time>
+                <FiCalendar /> 15 Mar 2021
+              </time>
+              <span>
+                <FiUser /> Joseph Oliveira
+              </span>
+            </div>
+          </a>
+          <a href="#">
+            <strong>Como utilizar hooks</strong>
+            <p>Pensando em sincronização em vez de ciclos de vida.</p>
+            <div>
+              <time>
+                <FiCalendar /> 15 Mar 2021
+              </time>
+              <span>
+                <FiUser /> Joseph Oliveira
+              </span>
+            </div>
+          </a>
+          <a href="#">
+            <strong>Como utilizar hooks</strong>
+            <p>Pensando em sincronização em vez de ciclos de vida.</p>
+            <div>
+              <time>
+                <FiCalendar /> 15 Mar 2021
+              </time>
+              <span>
+                <FiUser /> Joseph Oliveira
+              </span>
+            </div>
+          </a>
+          <a href="#">
+            <strong>Como utilizar hooks</strong>
+            <p>Pensando em sincronização em vez de ciclos de vida.</p>
+            <div>
+              <time>
+                <FiCalendar /> 15 Mar 2021
+              </time>
+              <span>
+                <FiUser /> Joseph Oliveira
+              </span>
+            </div>
+          </a>
+        </div>
+      </main>
+    </>
+  );
+}
 
 // export const getStaticProps = async () => {
 //   // const prismic = getPrismicClient();
